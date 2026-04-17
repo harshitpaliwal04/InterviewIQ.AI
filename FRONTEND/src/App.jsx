c08fcd4c19ade22743bdcb2bc3cf3eadd339c1fb
Harshit Paliwal <113759731+harshitpaliwal04@users.noreply.github.com>
2026-04-17 14:52:05 +0530

    Update App.jsx

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -9,7 +9,7 @@ import Report from './PAGES/InterviewReport.jsx'
 import { ProtectedRoutes, AuthRoute } from "./PAGES/ProtectedRoutes.jsx"
 import { useEffect } from 'react'
 
-export const ServerUrl = "https://interviewiq-ai-ymni.onrender.com"
+export const ServerUrl = ""
 
 function App() {
   useEffect(() => {
