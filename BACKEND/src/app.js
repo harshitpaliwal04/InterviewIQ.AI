ace0625bbbc4bfd162fc8ce1d314b0850499b851
Harshit Paliwal <113759731+harshitpaliwal04@users.noreply.github.com>
2026-04-17 14:53:46 +0530

    Update app.js

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 app.use(cors({
-    origin: "https://frontend-interviewiq-ai.onrender.com",
+    origin: "",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
