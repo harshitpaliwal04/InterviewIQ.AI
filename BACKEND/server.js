3783eade6b7d2bb5dabbab6895b070dfdf96b009
Harshit Paliwal <113759731+harshitpaliwal04@users.noreply.github.com>
2026-04-17 13:32:59 +0530

    Update server to listen on external URL

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const app = require('./src/app');
 const ConnectDB = require('./src/DATABASE/DB');
+const PORT = "https://frontend-interviewiq-ai.onrender.com";
 
-
-app.listen(3000, async ()=>{
+app.listen(PORT, async ()=>{
         await ConnectDB();
         console.log("Server is running ✅")
 })
