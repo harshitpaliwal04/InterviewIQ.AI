9526ae182b69cfcfc72ebf227ec9fb58cd6347ac
Harshit Paliwal <113759731+harshitpaliwal04@users.noreply.github.com>
2026-04-17 14:20:17 +0530

    Change server port to 3000 from URL

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
 const ConnectDB = require('./src/DATABASE/DB');
-const PORT = "https://frontend-interviewiq-ai.onrender.com";
 
-app.listen(PORT, async ()=>{
+app.listen(3000, async ()=>{
         await ConnectDB();
         console.log("Server is running ✅")
 })
